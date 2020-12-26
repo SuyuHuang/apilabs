527ec579e984c79e928026f607468ff8455ab621
SuyuHuang <764694182@163.com>
2020-12-26 22:56:15 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -4,6 +4,11 @@ import jwt from 'jsonwebtoken';
 import movieModel from "../movies/movieModel"
 const router = express.Router(); // eslint-disable-line
 
+
+function getpassword(pwd){
+  　var reg =/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{5,}$/
+  return reg.test(pwd); 
+}
 // Get all users
 router.get('/', (req, res,next) => {
     User.find().then(users =>  res.status(200).json(users)).catch(next);
@@ -18,10 +23,18 @@ router.post('/', async (req, res, next) => {
   }
   if (req.query.action === 'register') {
     await User.create(req.body).catch(next);
+    if(!getpassword(req.body.password)){
+      res.status(401).json({
+        code: 402,
+        msg: 'Wrong password format.'
+      });
+    }
+    else{
     res.status(201).json({
       code: 201,
       msg: 'Successful created new user.',
     });
+  }
   } else {
     const user = await User.findByUserName(req.body.username).catch(next);
       if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
@@ -60,13 +73,33 @@ router.put('/:id',  (req, res) => {
     .then(user => res.json(200, user));
 });
 router.post('/:userName/favourites', async (req, res, next) => {
+
   const newFavourite = req.body.id;
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
-  await user.save(); 
-  res.status(201).json(user); 
+  
+  if(user.favourites.includes(movie._id)){
+    res.status(402).json({
+      success: false,
+      msg: 'The movie has been added to the favourite.',
+    });
+  }
+  else if (!req.body.id ||!movie) {
+    res.status(401).json({
+      success: false,
+      msg: 'Please enter the valid id.',
+    });
+  }
+  else{
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
+  }
+
+  
+
+
 });
 
 router.get('/:userName/favourites', (req, res, next) => {
