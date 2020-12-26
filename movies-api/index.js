582e4a8b5a79faf073efca28a6c2cd1513603fa3
SuyuHuang <764694182@163.com>
2020-12-26 21:59:02 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
 import genresRouter from './api/genres'
 import session from 'express-session';
@@ -24,6 +24,7 @@ const app = express();
 const port = process.env.PORT;
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();
 }
 
 app.use(session({
